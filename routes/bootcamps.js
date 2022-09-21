0f8407de3e42117d7b9f95d680d4787330b7c746
Abhijeet Patil <patilabhijeet.ap@gmail.com>
2022-09-21 21:06:11 +0530

    basic router setup

---
+++ routes/bootcamps.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({ msg: 'working' });
+});
+
+router.get('/:id', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+router.put('/:id', (req, res) => {});
+
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
