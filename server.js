be389a65fdc32fd37419be20ac5c0707aecfc995
Abhijeet Patil <patilabhijeet.ap@gmail.com>
2022-09-21 22:51:47 +0530

    bootcamp model setup

--- server.js
+++ server.js
@@ -13,10 +13,20 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Mount routes
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+// Handle unhandled promise reject
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close server
+  server.close(() => process.emit(1));
 });
