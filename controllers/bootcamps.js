af9ebcaf7f230b256052daa0e3c5cb8ba270cee9
Abhijeet Patil <patilabhijeet.ap@gmail.com>
2022-09-21 21:18:27 +0530

    controller setup

---
+++ controllers/bootcamps.js
@@ -0,0 +1,19 @@
+exports.getBootcamps = (req, res, next) => {
+  res.json({ msg: 'working' });
+};
+
+exports.getBootcamp = (req, res, next) => {
+  res.json({ msg: 'working' });
+};
+
+exports.createBootcamp = (req, res, next) => {
+  res.json({ msg: 'working' });
+};
+
+exports.updateBootcamp = (req, res, next) => {
+  res.json({ msg: 'working' });
+};
+
+exports.deleteBootcamp = (req, res, next) => {
+  res.json({ msg: 'working' });
+};
