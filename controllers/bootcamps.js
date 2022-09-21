777369c03cc6c14f6369c7b57430da2a5b84b0ca
Abhijeet Patil <patilabhijeet.ap@gmail.com>
2022-09-21 23:24:41 +0530

    basic CRUD added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,19 +1,58 @@
-exports.getBootcamps = (req, res, next) => {
-  res.json({ msg: 'working' });
+const Bootcamp = require('../models/Bootcamp');
+
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.find();
+    res.status(200).json({ data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ error: true });
+  }
 };
 
-exports.getBootcamp = (req, res, next) => {
-  res.json({ msg: 'working' });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({ error: true });
+    }
+    res.status(200).json({ data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ error: true });
+  }
 };
 
-exports.createBootcamp = (req, res, next) => {
-  res.json({ msg: 'working' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({ data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ error: true });
+  }
 };
 
-exports.updateBootcamp = (req, res, next) => {
-  res.json({ msg: 'working' });
+exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, {
+      new: true,
+      runValidators: true,
+    });
+    if (!bootcamp) {
+      return res.status(400).json({ error: true });
+    }
+    res.status(200).json({ data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ error: true });
+  }
 };
 
-exports.deleteBootcamp = (req, res, next) => {
-  res.json({ msg: 'working' });
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({ error: true });
+    }
+    res.status(200).json({ data: {} });
+  } catch (error) {
+    res.status(400).json({ error: true });
+  }
 };
