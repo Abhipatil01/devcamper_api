471f7b44dec9a8f38f2e874deab8e81805a77e25
Abhijeet Patil <patilabhijeet.ap@gmail.com>
2022-09-21 21:54:52 +0530

    DB setup

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // load env vars
 dotenv.config({ path: './config/config.env' });
+
+// connect to DB
+connectDB();
+
+// Route
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
